5f98bc192c10c0906694b1048c6bc8c5dbf49cd4
soham <sohamcool411@gmail.com>
2024-09-16 22:51:18 +0530

    customform input change

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -62,7 +62,7 @@ export const PatientForm = () => {
           control={form.control}
           name="name"
           label="Full name"
-          placeholder="John Doe"
+          placeholder="Soham Patil"
           iconSrc="/assets/icons/user.svg"
           iconAlt="user"
         />
@@ -72,7 +72,7 @@ export const PatientForm = () => {
           control={form.control}
           name="email"
           label="Email"
-          placeholder="johndoe@gmail.com"
+          placeholder="soham@gmail.com"
           iconSrc="/assets/icons/email.svg"
           iconAlt="email"
         />
@@ -82,7 +82,7 @@ export const PatientForm = () => {
           control={form.control}
           name="phone"
           label="Phone number"
-          placeholder="(555) 123-4567"
+          placeholder="(+91) 1234567890"
         />
 
         <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
